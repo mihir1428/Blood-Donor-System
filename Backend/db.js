f50e7db97d0490e8eca018524c1cda0f3f2efbab
Md Moshiur Rahman <mdmoshiurrahman1428@gmail.com>
2026-04-11 21:47:27 +0600

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,14 +1,23 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
+const db = mysql.createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: Number(process.env.MYSQLPORT),
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 db.getConnection((err, connection) => {
-  if (err) {
-    console.log("MySQL pool error:", err);
-  } else {
-    console.log("MySQL Pool Connected");
-    connection.release();
-  }
+    if (err) {
+        console.log("MySQL pool error:", err);
+    } else {
+        console.log("MySQL Pool Connected");
+        connection.release();
+    }
 });
 
 module.exports = db;
